741478f28571013a24e7ed5ecbce6d98aa6c377d
Christopher Schmidt <crschmidt@google.com>
2022-09-10 03:45:27 -0400

    Cleaning up some duplicate scripts.

--- giswork/startend_count.py
+++
@@ -1,6 +0,0 @@
-import csv
-#"tripduration","starttime","stoptime","start station id","start station name","start station latitude","start station longitude","end station id","end station name","end station latitude","end station longitude","bikeid","usertype","postal code"
-
-r = csv.DictReader(open("202207-bluebikes-tripdata.csv"))
-for row in r:
-        print(row)
